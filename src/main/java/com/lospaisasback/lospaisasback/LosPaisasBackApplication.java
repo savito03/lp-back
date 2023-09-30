fc3121d40ae98e14c14a9b8b3a95525e843e37a2
savito03 <savioromero2019@gmail.com>
2023-09-30 15:52:08 -0500

    first commit

---
+++ src/main/java/com/lospaisasback/lospaisasback/LosPaisasBackApplication.java
@@ -0,0 +1,13 @@
+package com.lospaisasback.lospaisasback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LosPaisasBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LosPaisasBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lospaisasback/lospaisasback/LosPaisasBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lospaisasback.lospaisasback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LosPaisasBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
